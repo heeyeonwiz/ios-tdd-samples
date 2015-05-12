bcd8d4ae9adbb75b0fdbd4ac1856a8226c3c98b3
Jian Lv <jlv@thoughtworks.com>
2015-05-12 11:05:35 +0800

    Initial Commit

---
+++ tdd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tdd
+//
+//  Created by Jian Lv on 5/12/15.
+//  Copyright (c) 2015 Jian Lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tdd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tdd
+//
+//  Created by Jian Lv on 5/12/15.
+//  Copyright (c) 2015 Jian Lv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
